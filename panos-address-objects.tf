de9c7e91ff01fe7c27b8943ccebb5c778fcba8f1
gbenetatosprot <gbenetatosprot@gmail.com>
2025-06-04 11:35:57 -0500

    Update panos-address-objects.tf

--- panos-address-objects.tf
+++ panos-address-objects.tf
@@ -1,61 +1,39 @@
-# ========== FW1 ==========
-resource "panos_address_object" "fw1_rfc1918" {
+# ---------- FW1 ----------
+
+resource "panos_address_object" "fw1_objects" {
   for_each = local.address_objects
   provider = panos.fw1
 
   name        = each.key
   value       = each.value.value
   description = each.value.description
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
-resource "panos_address_group" "fw1_rfc1918_group" {
+resource "panos_address_group" "fw1_group" {
   provider = panos.fw1
 
   name        = "RFC-1918"
   description = "RFC-1918"
 
-  static_addresses = [
-    "rfc-1918-a",
-    "rfc-1918-b",
-    "rfc-1918-c"
-  ]
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  static_addresses = local.static_address_names
 }
 
-# ========== FW2 ==========
-resource "panos_address_object" "fw2_rfc1918" {
+# ---------- FW2 ----------
+
+resource "panos_address_object" "fw2_objects" {
   for_each = local.address_objects
   provider = panos.fw2
 
   name        = each.key
   value       = each.value.value
   description = each.value.description
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
 
-resource "panos_address_group" "fw2_rfc1918_group" {
+resource "panos_address_group" "fw2_group" {
   provider = panos.fw2
 
   name        = "RFC-1918"
   description = "RFC-1918"
 
-  static_addresses = [
-    "rfc-1918-a",
-    "rfc-1918-b",
-    "rfc-1918-c"
-  ]
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  static_addresses = local.static_address_names
 }
