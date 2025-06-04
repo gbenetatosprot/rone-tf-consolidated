80952818c18e35fbd14634a8207fe484a7466a18
gbenetatosprot <gbenetatosprot@gmail.com>
2025-06-04 11:38:42 -0500

    Update panos-address-objects.tf

--- panos-address-objects.tf
+++ panos-address-objects.tf
@@ -15,7 +15,16 @@ resource "panos_address_group" "fw1_group" {
   name        = "RFC-1918"
   description = "RFC-1918"
 
-  static_addresses = local.static_address_names
+  static_addresses = [
+    for k in local.static_address_names :
+    panos_address_object.fw1_objects[k].name
+  ]
+
+  depends_on = [panos_address_object.fw1_objects]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 # ---------- FW2 ----------
@@ -35,5 +44,14 @@ resource "panos_address_group" "fw2_group" {
   name        = "RFC-1918"
   description = "RFC-1918"
 
-  static_addresses = local.static_address_names
+  static_addresses = [
+    for k in local.static_address_names :
+    panos_address_object.fw2_objects[k].name
+  ]
+
+  depends_on = [panos_address_object.fw2_objects]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
