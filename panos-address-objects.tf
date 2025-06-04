f67852c0ef9f3c6111c1bfe06bda86b19b71a784
gbenetatosprot <gbenetatosprot@gmail.com>
2025-06-04 11:29:45 -0500

    Update panos-address-objects.tf

--- panos-address-objects.tf
+++ panos-address-objects.tf
@@ -1,21 +1,10 @@
-resource "panos_address_object" "rfc1918" {
-  for_each = {
-    for combo in flatten([
-      for fw_key, fw in var.firewalls : [
-        for obj_key, obj in local.address_objects : {
-          key         = "${fw_key}_${obj_key}"
-          fw_key      = fw_key
-          obj_name    = obj_key
-          value       = obj.value
-          description = obj.description
-        }
-      ]
-    ]) : combo.key => combo
-  }
+# ==================== FW1 ====================
+resource "panos_address_object" "fw1_rfc1918" {
+  for_each = local.address_objects
 
-  provider = local.panos_providers[each.value.fw_key]
+  provider = panos.fw1
 
-  name        = each.value.obj_name
+  name        = each.key
   value       = each.value.value
   description = each.value.description
 
@@ -24,10 +13,40 @@ resource "panos_address_object" "rfc1918" {
   }
 }
 
-resource "panos_address_group" "rfc1918_group" {
-  for_each = var.firewalls
+resource "panos_address_group" "fw1_rfc1918_group" {
+  provider = panos.fw1
 
-  provider = local.panos_providers[each.key]
+  name        = "RFC-1918"
+  description = "RFC-1918"
+
+  static_addresses = [
+    "rfc-1918-a",
+    "rfc-1918-b",
+    "rfc-1918-c"
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# ==================== FW2 ====================
+resource "panos_address_object" "fw2_rfc1918" {
+  for_each = local.address_objects
+
+  provider = panos.fw2
+
+  name        = each.key
+  value       = each.value.value
+  description = each.value.description
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "panos_address_group" "fw2_rfc1918_group" {
+  provider = panos.fw2
 
   name        = "RFC-1918"
   description = "RFC-1918"
