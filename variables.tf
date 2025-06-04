e5ac52b03ba638ac9ac54a80819faecfc067a6ed
gbenetatosprot <gbenetatosprot@gmail.com>
2025-06-04 11:17:48 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,10 +3,20 @@ variable "firewalls" {
     hostname = string
     username = string
   }))
+  default = {
+    fw1 = {
+      hostname = "52.203.138.190"
+      username = "admin"
+    },
+    fw2 = {
+      hostname = "3.215.31.89"
+      username = "admin"
+    }
+  }
 }
 
 variable "panos_password" {
   type      = string
   sensitive = true
-  default = "ProTEr@2024"
+  default   = "ProTEr@2024"
 }
